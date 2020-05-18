501989578ec31a36724187368e036c23dee7fd67
hillarioh <okerioh@gmail.com>
2020-05-18 17:43:12 +0300

    test 0

---
+++ 0.sql
@@ -0,0 +1,15 @@
+-- Query to show the population of Germany
+SELECT population
+FROM world
+WHERE name = 'Germany';
+
+-- Query to Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
+SELECT name, population
+FROM world
+WHERE name IN ('Sweden', 'Norway', 'Denmark');
+
+-- Query to show the country and the area for countries with an area between 200,000 and 250,000
+SELECT name, area
+FROM world
+WHERE area BETWEEN 200000 AND 250000;
+
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SQL-ZOO
+
 The Odin Project- SQL
+
+Project: SQL
+This is the final project from 'The Odin Project' curriculum on databases and SQL.
+
+-Tutorials 0-9 listed under the “Tutorial Section” completed
+
+Useful Links
+[Link to The Odin Project assignment page](https://www.theodinproject.com/courses/databases/lessons/sql)
