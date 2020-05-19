4ac8aedfe2d8d319c2d1d6fafdb7864d5d2f783d
hillarioh <okerioh@gmail.com>
2020-05-19 07:28:31 +0300

    edit 1.sql file

--- 1.sql
+++ 1.sql
@@ -28,3 +28,51 @@ WHERE name LIKE '%United%'
 SELECT name, population, area
 FROM world
 WHERE area > 3000000 OR population > 250000000;
+
+-- Query to Show the countries that are big by area (more than 3 million) or big by population
+-- (more than 250 million) but not both. Show name, population and area.
+SELECT name, population, area
+FROM world
+WHERE population > 250000000
+    AND NOT area > 3000000
+;
+
+-- Query to Show name and population in millions and GDP in billions for the countries in continent 'South America'.
+SELECT name, ROUND(population/1000000,2), ROUND(gdp/1000000000,2)
+FROM world
+WHERE continent='South America';
+
+-- Query to Show the name and per-capita GDP for those countries with a GDP of at least one trillion
+-- (1000000000000; that is 12 zeros). Round this value to the nearest 1000.
+SELECT name, ROUND(gdp/population,-3)
+FROM world
+WHERE gdp >=1000000000000;
+
+-- Greece has capital Athens. 
+-- Each of the strings 'Greece', and 'Athens' has 6 characters.
+-- Query to Show the name and capital where the name and the capital have the same number of characters.
+SELECT name, capital
+FROM world
+WHERE LEN(name)=LEN(capital)
+
+-- The capital of Sweden is Stockholm. Both words start with the letter 'S'.
+-- Query to Show the name and the capital where the first letters of each match. 
+-- Don't include countries where the name and the capital are the same word.
+-- You can use the function LEFT to isolate the first character.
+-- You can use <> as the NOT EQUALS operator.
+SELECT name, capital
+FROM world
+WHERE (LEFT(name,1)=LEFT(capital,1)) AND name <> capital;
+
+-- Equatorial  Guinea and Dominican Republic have all of the vowels (a e i o u) in the name. 
+-- They don't count because they have more than one word in the name. Find the country that
+--  has all the vowels and no spaces in its name. 
+-- You can use the phrase name NOT LIKE '%a%' to exclude characters from your results.
+SELECT name
+FROM world
+WHERE name LIKE '%a%'
+    AND name LIKE '%e%'
+    AND name LIKE '%i%'
+    AND name LIKE '%o%'
+    AND name LIKE '%u%'
+    AND name NOT LIKE '% %';
